e853f698f6dc2493899c28948ab035aacb2e1a66
Sofya <sarksofya@yandex.ru>
2023-01-27 21:03:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [hotels, setHotels] = useState(data)
+  const [showText, setShowText] = useState(false)
+  
+  const removeHotel = (id) => {
+
+let newHotels = hotels.filter(hotel => hotel.id !== id)
+
+setHotels(newHotels)
+  }
+const showTextClick = (element) => {
+  element.showMore = !element.showMore;
+  setShowText(!showText)
+}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+     <div className='container'>
+      <h1>NYC TOP {hotels.length}  HOTELS</h1>
+      </div>
+      {hotels.map((element => {
+        const {id, hotelName, description, img, source, showMore} = element;
+
+        return(
+      <div  key={id}>
+      <div className='container'>
+        <h2>{id} - {hotelName}</h2>
+      </div>
+      <div className='container'>
+      
+        <p> {showMore ? description : description.substring(0, 190) + "..."} 
+        <button onClick={() => showTextClick(element)}>{showMore ? "Show less" : "Show more"}</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      </div>
+      <div className='container'>
+        <img src={img} width="500px" alt="Hotel in NYC"/>
+      </div>
+      <div className='container'>
+        <p>Source: {source}</p>
+        
+      </div>
+      <div className='container'>
+        <button className='btn' onClick={() => removeHotel(id)}>Remove</button>
+      </div>  
     </div>
-  );
+    )
+  }))}
+  </div>
+  )
 }
 
 export default App;
